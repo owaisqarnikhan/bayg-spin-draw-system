56bafb422f6be1a04cc66309533730f32e42b2cc
Muhammad Owais Qarni Khan <110786215+owaisqarnikhan@users.noreply.github.com>
2025-11-27 21:47:48 +0300

    Simplify Replit configuration to avoid nix build issues

--- replit.nix
+++ replit.nix
@@ -1,8 +1,5 @@
 { pkgs }: {
   deps = [
-    pkgs.nodejs-18_x
-    pkgs.nodePackages.typescript
-    pkgs.nodePackages.typescript-language-server
-    pkgs.nodePackages.prisma
+    pkgs.nodejs_20
   ];
 }
